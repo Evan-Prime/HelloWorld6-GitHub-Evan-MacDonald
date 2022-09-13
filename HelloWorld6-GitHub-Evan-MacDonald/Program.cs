8b72e4ab5491e4696b4a1408e4f3d2dda83c606d
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-09-13 09:17:54 -0300

    Initial commit

---
+++ HelloWorld6-GitHub-Evan-MacDonald/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld6_GitHub_Evan_MacDonald
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 6");
+            Console.WriteLine("");
+            Console.WriteLine("By: Evan MacDonald");
+            Console.WriteLine("");
+            Console.ReadKey(true);
+        }
+    }
+}
